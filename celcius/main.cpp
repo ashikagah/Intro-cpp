5ef65ed62274b5f308c3a33c369c4301f4b9dec4
Hiroshi Ashikaga <hashika1@jhmi.edu>
2017-05-15 16:59:56 -0400

    initial commit

---
+++ celcius/main.cpp
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+int main ()
+{
+    float FTemp = 0;
+    float CTemp = 0;
+    
+    cout << "Enter a Farenheit temperature: ";
+    cin>>FTemp;
+    
+    CTemp = (FTemp - 32.0) / (9.0/5.0);
+    cout << "\n"<<FTemp <<" in farenheit = " <<CTemp<<" in Celcius\n";
+    return 0;
+}
